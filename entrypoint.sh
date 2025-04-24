b5d3306bebad0ff34c7ec0c294239abf3411f50d
ElgunRustemli <elgunrustemli99@gmail.com>
2025-04-24 10:15:26 +0400

    Initial commit for Railway deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-python manage.py migrate
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Starting server on port $PORT..."
 exec gunicorn wcu_check.wsgi:application --bind 0.0.0.0:$PORT
