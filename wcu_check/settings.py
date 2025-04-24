34e51ca3a2d2f376cad0f7ed647420ede948c887
ElgunRustemli <elgunrustemli99@gmail.com>
2025-04-24 17:17:40 +0400

    Initial commit for Railway deploy

--- wcu_check/settings.py
+++ wcu_check/settings.py
@@ -11,6 +11,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # WhiteNoise üçün
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
